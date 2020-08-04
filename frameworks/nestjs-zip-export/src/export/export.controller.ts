b47841ab58e76a0ca2732a719da6d81dd17e5c77
Alexander Buyanov <buyanov15@gmail.com>
2020-08-04 10:20:52 +0200

    Create zip

--- frameworks/nestjs-zip-export/src/export/export.controller.ts
+++ frameworks/nestjs-zip-export/src/export/export.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Header, Res, Response } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from 'fs';
-// import * as JSZip from 'jszip';
+import * as archiver from 'archiver';
 
 @Controller('export')
 export class ExportController {
@@ -9,9 +9,28 @@ export class ExportController {
   @Header('Content-Type', 'application/zip')
   @Header('Content-Disposition', 'attachment; filename=test.zip')
   exportZip(@Res() res: Response): any {
-    const stream = fs.createReadStream(path.resolve(__dirname, '../../test.zip'));
+    const archive = archiver('zip');
 
+    // error handling
+    archive.on('error', (err) => {
+      throw err;
+    });
+
+    // end zipping
+    archive.on('end', function() {
+      console.log('---------- Archive wrote %d bytes', archive.pointer());
+    });
+
+    // json to export
+    const jsonToZip = {"Hello": "World"};
+
+    archive.append(JSON.stringify(jsonToZip), { name: 'someJsonExport.json' });
+    archive.finalize(); 
+
+    // const stream = fs.createReadStream(path.resolve(__dirname, '../../test.zip'));
+
+    // stream magic
     // @ts-ignore
-    stream.pipe(res);
+    archive.pipe(res);
   }
 }
