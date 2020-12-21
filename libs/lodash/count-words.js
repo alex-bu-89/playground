a3bf68646c29023b079e2367a48d65e6d5e6830d
Alexander Buyanov <buyanov15@gmail.com>
2020-12-21 17:27:42 +0100

    Add 5 words playground

---
+++ libs/lodash/count-words.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import lukomorie from './lukomorie.js';
+
+const top5Words = _.flow([
+    _.toLower,
+    _.words,
+    _.countBy,
+    _.toPairs,
+    _.partial(_.orderBy, _, 1, 'desc'),
+    _.partial(_.take, _, 5),
+    _.fromPairs,
+]);
+
+console.log('------------>', top5Words(lukomorie));
