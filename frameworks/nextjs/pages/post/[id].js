3dbfa8a213d61016d6ed23bce2f03b9417b72868
Alexander Buyanov <buyanov15@gmail.com>
2020-12-23 16:38:33 +0100

    Load posts from mock server

--- frameworks/nextjs/pages/post/[id].js
+++ frameworks/nextjs/pages/post/[id].js
@@ -1,12 +1,61 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import MainLayout from '../../components/MainLayout';
 
-export default function Post() {
+function Post({ post: serverPost }) {
+    const [post, setPost] = useState(serverPost);
     const router = useRouter();
+
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`http://localhost:4200/posts/${router.query.id}`);
+            const data = await response.json();
+            setPost(data);
+        }
+
+        if (!serverPost) load();
+    }, []);
+
+    if (!post) {
+       return (
+            <MainLayout>
+                <p>Loading...</p>
+            </MainLayout>
+       )
+    }
 
     return (
         <MainLayout>
-            <h1>Post { router.query.id }</h1>
+            <h1>{ post.title }</h1>
+            <p>{ post.body }</p>
+            <Link href="/posts"><a>Go back to posts</a></Link>
         </MainLayout>
     )
 }
+
+// export async function getServerSideProps({ query, req }) {
+//     const response = await fetch(`http://localhost:4200/posts/${query.id}`);
+//     const post = await response.json();
+
+//     return {
+//         props: {
+//             post
+//         },
+//     };
+// }
+
+Post.getInitialProps = async ({ query, req }) => {
+    if (!req) {
+        return { post: null }
+    }
+
+    const response = await fetch(`http://localhost:4200/posts/${query.id}`);
+    const post = await response.json();
+
+    return {
+        post,
+    };
+};
+
+export default Post;
--- frameworks/nextjs/pages/posts.js
+++ frameworks/nextjs/pages/posts.js
@@ -1,10 +1,57 @@
-import Head from 'next/head';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
 import MainLayout from '../components/MainLayout';
 
-export default function Posts() {
+function Posts({ posts: serverPosts }) {
+    const [posts, setPosts] = useState(serverPosts);
+
+    useEffect(() => {
+        async function load() {
+            const response = await fetch('http://localhost:4200/posts');
+            const posts = await response.json();
+            setPosts(posts);
+        }
+
+        if (!serverPosts) { load(); }
+    }, []);
+
+    if (!posts) {
+        return (
+             <MainLayout>
+                 <p>Loading...</p>
+             </MainLayout>
+        )
+     }
+
     return (
         <MainLayout title="Posts page">
             <h1>Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Link href={'/post/[id]'} as={`/post/${post.id}`} >
+                            <a>{post.title}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </MainLayout>
     );
 }
+
+Posts.getInitialProps = async ({ req }) => {
+    if (!req) {
+        return {
+            posts: null,
+        }
+    }
+
+    const response = await fetch('http://localhost:4200/posts');
+    const posts = await response.json();
+
+    return {
+        posts,
+    };
+};
+
+export default Posts;
