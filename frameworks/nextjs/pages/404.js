3388fc46d825fd7ccd4df5bccb1246df738be743
Alexander Buyanov <buyanov15@gmail.com>
2020-12-23 15:39:08 +0100

    Add Sass

--- frameworks/nextjs/pages/404.js
+++ frameworks/nextjs/pages/404.js
@@ -1,11 +1,11 @@
 import Link from 'next/link';
-import Head from 'next/head';
 import { MainLayout } from '../components/MainLayout';
+import classes from '../styles/error.module.scss'
 
 export default function Error() {
     return (
         <MainLayout title="Error Page">
-            <h1>Error 404</h1>
+            <h1 className={classes.error}>Error 404</h1>
             <p>Please go back to <Link href="/"><a>safety</a></Link></p>
         </MainLayout>
     );
