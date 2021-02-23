5094d7e626e5bc6b874431d99af69904cd563ea1
Alexander Buyanov <buyanov15@gmail.com>
2021-02-23 18:02:54 +0100

    Finish styling; use activeClassName for the nav

--- frameworks/gatsby-landing-page/src/components/Layout/Header/Header.js
+++ frameworks/gatsby-landing-page/src/components/Layout/Header/Header.js
@@ -4,18 +4,20 @@ import { Link } from 'gatsby';
 import styles from './Header.module.scss';
 
 const ListLink = (props) => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link className={styles.link} to={props.to}>{props.children}</Link>
+  <li className={styles.navItem}>
+    <Link className={styles.navLink} activeClassName={styles.activeNavLink} to={props.to}>
+      {props.children}
+    </Link>
   </li>
 );
 
 const Layout = () => {
   return (
     <header className={styles.header}>
-      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+      <Link to="/" className={styles.logo}>
         <h3 style={{ display: `inline` }}>MySweetSite</h3>
       </Link>
-      <nav style={{ listStyle: `none`, float: `right` }}>
+      <nav className={styles.nav}>
         <ListLink to="/">Home</ListLink>
         <ListLink to="/about/">About</ListLink>
         <ListLink to="/contact/">Contact</ListLink>
