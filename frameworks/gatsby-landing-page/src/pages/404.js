6b3db7470f2586cb607822b32c9f848401fc3f39
Alexander Buyanov <buyanov15@gmail.com>
2021-02-24 09:12:41 +0100

    Add 404 page

---
+++ frameworks/gatsby-landing-page/src/pages/404.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Layout from '../components/Layout/Layout';
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>404 not found</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default IndexPage;
