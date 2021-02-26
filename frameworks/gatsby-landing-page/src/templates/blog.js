d9c01d7be851287d530340441ab753b7066771b5
Alexander Buyanov <buyanov15@gmail.com>
2021-02-26 17:13:08 +0100

    Add content for dynamic created pages

--- frameworks/gatsby-landing-page/src/templates/blog.js
+++ frameworks/gatsby-landing-page/src/templates/blog.js
@@ -1,12 +1,25 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/Layout/Layout';
 
-const Blog = () => {
-  return (
-    <Layout>
-      this is the blog template
-    </Layout>
-  );
+const Blog = ({ data }) => {
+  return <Layout>
+    <h1>{data.markdownRemark.frontmatter.title}</h1>
+    <p>{data.markdownRemark.frontmatter.date}</p>
+    <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
+  </Layout>;
 };
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`;
 
 export default Blog;
